47dbf06b339bb8b54b24830e06c93ae2e0789410
leocastroz <castroz.dev@gmail.com>
2023-06-20 11:21:55 -0300

    feat: Using "Tailwind" as global CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+// @ts-check
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
