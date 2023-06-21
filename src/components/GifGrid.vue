da39ed6b844700e9880f51c65f7e02552a8051fc
leocastroz <castroz.dev@gmail.com>
2023-06-21 12:12:03 -0300

    docs: Adding tooltip to images

--- src/components/GifGrid.vue
+++ src/components/GifGrid.vue
@@ -1,4 +1,3 @@
-
 <script lang="ts">
 import axios from 'axios'
 import { saveAs } from 'file-saver'
@@ -11,30 +10,40 @@ export default {
     }
   },
   methods: {
-		downloadImage(url) {
+    downloadImage(url) {
       axios
         .get(url, {
           responseType: 'blob'
         })
-        .then(response =>  {
+        .then((response) => {
           const filename = 'image.gif'
           saveAs(response.data, filename)
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error)
-        });
+        })
     },
     openModal(gif) {
       this.$emit('open-modal', gif)
     }
   }
-};
+}
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-    <div v-for="(gif, index) in filteredGifs" :key="index" class="rounded-lg cursor-pointer p-4 m-4 hover:scale-110 transition-transform duration-300">
-				<img src="../assets/extras/download.svg" alt="donwload" class="bg-gradient-to-r from-pink-500 to-purple-700 text-white rounded-sm w-6 p-1 my-1" @click="downloadImage(gif.images.original.url)">
+    <div v-for="(gif, index) in filteredGifs" :key="index"
+      class="rounded-lg cursor-pointer p-4 m-4 hover:scale-110 transition-transform duration-300">
+      
+      <VTooltip :distance="-25" :skidding="0" class="rteste">
+        <img src="../assets/extras/download.svg" alt="donwload"
+          class="bg-gradient-to-r from-pink-500 to-purple-700 text-white rounded-sm w-6 p-1 my-1"
+          @click="downloadImage(gif.images.original.url)" />
+        <template #popper>
+          <p class="text-xs text-purple-500 font-bold">Donwload GIF</p>
+        </template>
+      </VTooltip>
+
       <div class="relative">
         <img :src="gif.images.original.url" alt="Gif" class="w-lg rounded-lg fist-image" @click="openModal(gif)" />
         <p class="text-xs font-semibold text-zinc-500 text-center p-2">{{ gif.title }}</p>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import axios from 'axios'
 import store from './database/store'
+import FloatingVue  from 'floating-vue'
+import 'floating-vue/dist/style.css'
 
 import App from './App.vue'
 import router from './router'
@@ -13,6 +15,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(store)
+app.use(FloatingVue);
 
 app.config.globalProperties.$axios = axios
 
