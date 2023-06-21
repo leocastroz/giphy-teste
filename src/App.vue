6d2b7be18a2bd23d47be1a54e5d8a5f3c974bcfd
leocastroz <castroz.dev@gmail.com>
2023-06-21 04:10:45 -0300

    refact: Cleaning up main component "app.vue"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import MainComponent from './components/MainComponent.vue'
+import './assets/config/config_app.css'
 </script>
 
 <template>
@@ -9,7 +10,7 @@ import MainComponent from './components/MainComponent.vue'
       <MainComponent />
     </div>
     <nav
-      class="max-w-xs mx-auto shadow-lg border-solid border-2 border-pink-600 bg-black rounded-full text-white text-center mt-8 p-5">
+      class="max-w-xs mx-auto shadow-lg border-solid border-2 border-pink-600 bg-black rounded-full text-white text-center mt-8 p-5 nav-bar">
       <div class="nav-links">
         <RouterLink to="/" class="text-white font-bold mr-4">HOME</RouterLink>
         <RouterLink to="/giphy" class="text-white font-bold">FOR GIPHY</RouterLink>
@@ -18,17 +19,3 @@ import MainComponent from './components/MainComponent.vue'
   </section>
   <RouterView />
 </template>
-
-
-<style scoped>
-nav {
-  box-shadow: 0 0 1000px 5px #FF0080;
-}
-
-nav a.router-link-exact-active {
-  background: linear-gradient(270deg, #FF0080 0%, #9433f5 100%);
-  -webkit-text-fill-color: transparent;
-  -webkit-background-clip: text;
-  background-clip: text;
-}
-</style>
