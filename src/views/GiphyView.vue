6699a7e53910b06e76fbaf556b28edb3a541992e
leocastroz <castroz.dev@gmail.com>
2023-06-20 22:08:55 -0300

    refact: Improving ".env" environment variable

--- src/views/GiphyView.vue
+++ src/views/GiphyView.vue
@@ -38,10 +38,14 @@ export default {
 
   methods: {
     async loadGifs() {
+      const queryString = new URLSearchParams({
+        api_key: import.meta.env.VITE_APP_API_KEY,
+        offset: this.offset
+      })
       try {
         this.loading = true
         const response = await axios.get(
-          `https://api.giphy.com/v1/gifs/trending?api_key=ICR7VcGaD4CwjRQ3LvYkpPwG4VHdG5pM&offset=${this.offset}`
+          `${import.meta.env.VITE_URL_GIPHY}?${queryString.toString()}`
         )
         this.gifs = [...this.gifs, ...response.data.data]
         this.originalGifs = [...this.gifs]
@@ -89,29 +93,45 @@ export default {
 <template>
   <section>
     <div class="flex justify-center items-center">
-      <input type="text" v-model="searchQuery" @input="filterGifs" placeholder="Search Gifs"
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="filterGifs"
+        placeholder="Search Gifs"
         class="bg-gradient-to-r from-pink-500 to-purple-700 border-2 border-white rounded-lg py-2 px-4 m-8 text-white focus:font-medium focus:outline-none"
       />
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
-      <div v-for="(gif, index) in filteredGifs" :key="index"
+      <div
+        v-for="(gif, index) in filteredGifs"
+        :key="index"
         class="rounded-lg cursor-pointer p-4 m-4 hover:scale-110 transition-transform duration-300"
       >
         <img :src="gif.images.original.url" alt="Gif" class="w-lg" @click="openModal(gif)" />
       </div>
     </div>
 
-    <div v-if="loading" class="flex justify-center items-center ">
-      <div class="animate-spin rounded-full h-10 w-10 border-dashed border-2 border-white border-t-5 border-b-2 bg-gradient-to-r from-purple-700 to-pink-700"></div>
+    <div v-if="loading" class="flex justify-center items-center">
+      <div
+        class="animate-spin rounded-full h-10 w-10 border-dashed border-2 border-white border-t-5 border-b-2 bg-gradient-to-r from-purple-700 to-pink-700"
+      ></div>
     </div>
-    <div class="error mt-10 text-3xl text-center text-pink-500" v-else-if="error">Ocorreu um erro ao carregar os gifs.</div>
+    <div class="error mt-10 text-3xl text-center text-pink-500" v-else-if="error">
+      Ops.. Ocorreu um erro ao carregar os Gifs.
+    </div>
 
-    <div class="modal fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 flex items-center justify-center" v-if="modalOpen">
+    <div
+      class="modal fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 flex items-center justify-center"
+      v-if="modalOpen"
+    >
       <div class="modal-content">
         <img :src="selectedGif.images.original.url" alt="Gif" class="w-full" />
         <div class="close-button absolute top-4 right-4">
-          <span class="material-symbols-outlined cursor-pointer text-3xl text-white bg-gradient-to-r from-pink-500 to-purple-700 rounded-full py-1 px-2" @click="closeModal">
+          <span
+            class="material-symbols-outlined cursor-pointer text-3xl text-white bg-gradient-to-r from-pink-500 to-purple-700 rounded-full py-1 px-2"
+            @click="closeModal"
+          >
             cancel
           </span>
         </div>
@@ -121,7 +141,6 @@ export default {
 </template>
 
 <style scoped>
-
 .hover\:scale-110:hover {
   transform: scale(1.1);
 }
