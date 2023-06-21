75bc8c77dcacc7d67a8590f249d0be7c2eaf03c6
leocastroz <castroz.dev@gmail.com>
2023-06-21 00:54:08 -0300

    refact: Defaulting "export" to TS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-// @ts-check
 /** @type {import('tailwindcss').Config} */
 
-// eslint-disable-next-line no-undef
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
+export const content = ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
+export const theme = {
+  extend: {}
+};
+export const plugins = []
